60500c5f62423631da36a567e89aadc218081929
Eva Leonard <evaleonard@Eva-Leonards-MacBook-Pro.local>
2014-01-30 20:09:50 +0000

    Initial Commit

---
+++ OpenGLApp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGLApp
+//
+//  Created by Eva Leonard on 30/01/2014.
+//  Copyright (c) 2014 Eva Leonard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
